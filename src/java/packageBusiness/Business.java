ac2816e75c39aab0f7b70ac2c557173185c5cd19
bavayemilien <bavayemilien@1f7dfc47-acab-4c49-a1b6-a7ba3655b872>
2012-12-06 15:48:45 +0000

    validation

---
+++ src/java/packageBusiness/Business.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package packageBusiness;
+
+/**
+ *
+ * @author Emilien
+ */
+public class Business {
+    
+    
+    public void addUser(String nom,String prenom,String rue,int numero,String boite,String localite,int codePostal, String email,String motDePasse)
+    {
+    
+    }
+    
+}
